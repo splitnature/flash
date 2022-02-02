8350e621b34b8828e046150922747a2d632b718d
Santiago Orozco <splitnature@protonmail.com>
2022-02-02 00:37:08 +0000

    first

--- src/front/js/pages/stduboard.js
+++ src/front/js/pages/stduboard.js
@@ -1,5 +1,16 @@
 import React from "react";
+import { card } from "../component/card";
 
 export const studentDashboard = () =>{
-    
+    <Card style={{ width: '18rem' }}>
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+    <Card.Text>
+        
+    </Card.Text>
+    <Card.Link href="#">Card Link</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+</Card>
 }
